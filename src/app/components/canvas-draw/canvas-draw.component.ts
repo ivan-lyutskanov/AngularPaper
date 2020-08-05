0e710db297bcfeb4867d617b44e0559515761cf5
ivan.lyutsknaov <ivan.lyutskanov@scalefocus.com>
2020-08-05 19:57:00 +0300

    base functionality

--- src/app/components/canvas-draw/canvas-draw.component.ts
+++ src/app/components/canvas-draw/canvas-draw.component.ts
@@ -1,5 +1,6 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { PaperScope, Path, Project } from 'paper';
+import { Component, ViewChild, ElementRef, AfterViewInit, Input } from '@angular/core';
+import { Raster, Path, Project } from 'paper';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-canvas-draw',
@@ -9,49 +10,61 @@ import { PaperScope, Path, Project } from 'paper';
 export class CanvasDrawComponent implements AfterViewInit  {
   @ViewChild('canvas', {static: false }) canvas: ElementRef;
   // TODO: Fix types
-  scope: any;
   project: any;
   path: any;
+
+  raster: any;
+  @Input() imageURL: string;
+
+  @Input() strokeColor = '#c92b39'; // default red
+  @Input() strokeWidth = 1;
 
   constructor() {}
 
   ngAfterViewInit() {
-    this.scope = new PaperScope();
+    // Create new project / canvas
     this.project = new Project(this.canvas.nativeElement);
 
-    // this.path = new Path();
+    // Check if image url is provided
+    if (this.imageURL) {
+      // Draw the image on the canvas
+      this.raster = new Raster(this.imageURL);
+      // Center the image
+      this.raster.position = this.project.view.center;
+    }
 
-    // this.path.onMouseDown = function (event: any) {
-    //   console.log(event);
-    // };
-    // this.project.activeLayer.addChild(this.path);
+    // Bind mouse events
+    this.project.view.onMouseDown = this.onMouseDown.bind(this);
+    this.project.view.onMouseDrag = this.onMouseDrag.bind(this);
+    this.project.view.onMouseUp = this.onMouseUp.bind(this);
 
-    // draw a circle (test)
-    const circle = new Path.Circle({
-      center: this.scope.view.center,
-      radius: 80,
-      fillColor: 'red',
-    });
-
-    circle.onMouseDrag = this.onMouseDrag;
   }
 
-  // onMouseDown() {
-  //   this.path.strokeColor = "black";
-  // }
+  onMouseDown() {
+    // Create new path
+    this.path = new Path();
+    // Sets the stroke width
+    this.path.strokeWidth = this.strokeWidth;
+    // Sets the stroke color
+    this.path.strokeColor = this.strokeColor;
+  }
 
   onMouseDrag(event: any) {
     // Every drag event, add a point to the path at the current
     // position of the mouse:
-    // this.path.add(event.point);
-
-    // Move the circle
-    event.target.position = event.point;
-    // console.log(event);
+    this.path.add(event.point);
   }
 
   onMouseUp() {
     // When the mouse is released, simplify it:
-    // this.path.simplify();
+    this.path.simplify();
+  }
+
+  onSave() {
+
+    this.project.view.draw();
+    console.log(this.project.view.element);
+    // tslint:disable-next-line: only-arrow-functions
+    this.project.view.element.toBlob( function(blob: Blob) { saveAs(blob, 'image.png'); });
   }
 }
